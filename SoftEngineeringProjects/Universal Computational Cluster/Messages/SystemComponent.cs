5b560ef5696950eb088646ead3ce7337be0c13f7
Patryk Kujawski <patryk.kujawski93@gmail.com>
2015-03-24 15:00:41 +0100

    Aktualizacja dokumentacji w folderze Documentation Kosmetyczne zmiany

--- SoftEngineeringProjects/Universal Computational Cluster/Messages/SystemComponent.cs
+++ SoftEngineeringProjects/Universal Computational Cluster/Messages/SystemComponent.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,6 @@ namespace Common
     {
        protected Uri CommunicationServerAddress;
        protected ushort CommunicationServerPort;
+       
     }
 }
