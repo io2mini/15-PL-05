4117a11b93c757c69b69a129cc587b4991147352
bobson555 <bobkob@student.mini.pw.edu.pl>
2015-04-08 11:36:27 +0200

    Wstępna naprawa Status Report Generatora

--- SoftEngineeringProjects/Universal Computational Cluster/Common/Messages/Generators/StatusReportGenerator.cs
+++ SoftEngineeringProjects/Universal Computational Cluster/Common/Messages/Generators/StatusReportGenerator.cs
@@ -9,8 +9,10 @@ namespace Common.Messages.Generators
 {
     public class StatusReportGenerator
     {
+        
         /// <summary>
         /// Metoda generująca message typu Status
+        /// TODO: Przenieść to z powrotem, bo musi mieć dostęp do danych z wewnątrz komponentu (patrz: schema)
         /// </summary>
         /// <param name="ComponentId"></param>
         /// <returns>Status - Status Report komponentu</returns>
@@ -19,6 +21,11 @@ namespace Common.Messages.Generators
             if (ComponentId < 0) throw new NegativeIdException();
             var result = new Status();
             result.Id = ComponentId;
+            var threads = new List<StatusThread>();
+            var statusthread = new StatusThread();
+            statusthread.State = StatusThreadState.Idle;
+            threads.Add(statusthread);
+            result.Threads = threads.ToArray();
             return result;
         }
     }
