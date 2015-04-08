2d5d069b964c4b1da6f00a70a4f1ea6a5c52cb20
Sylwia Nowak <Sylwia.Nowak@cybercom.com>
2015-04-08 08:35:20 +0200

    nowa architektoru, szablony testów
    Change-Id: Ib7721a3704f2b0fbc364b3bb5b96af741be0c331

---
+++ SoftEngineeringProjects/Universal Computational Cluster/ComputationalClientTest/ComputationalClientTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputationalClientTest
+{
+    [TestClass]
+    public class ComputationalClientTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SoftEngineeringProjects/Universal Computational Cluster/ComputationalNodeTest/ComputationalNodeTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputationalNodeTest
+{
+    [TestClass]
+    public class ComputationalNodeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
